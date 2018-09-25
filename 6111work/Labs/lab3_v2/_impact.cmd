7b9f7f0f6d1eab5495ac9b8fbebbebc5586637a7
Magson Gao <magson@mit.edu>
2018-09-25 19:30:13 -0400

    rom still not working@

---
+++ 6111work/Labs/lab3_v2/_impact.cmd
@@ -0,0 +1,20 @@
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify 
+identifyMPM 
+assignFile -p 2 -file "/afs/athena.mit.edu/user/m/a/magson/Documents/6111/6111work/Labs/lab3_v2/lab3.bit"
+Program -p 2 
+saveProjectFile -file "/afs/athena.mit.edu/user/m/a/magson/Documents/6111/6111work/Labs/lab3_v2/lab3_v2.ipf"
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
