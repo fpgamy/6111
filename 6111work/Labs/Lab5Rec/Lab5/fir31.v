1ab2cd032adbfd8af42042bb3ddd5849b7d6d8fd
fpgamy <mg3115@ic.ac.uk>
2018-10-23 19:03:40 -0400

    attempt fir fix

--- 6111work/Labs/Lab5Rec/Lab5/fir31.v
+++ 6111work/Labs/Lab5Rec/Lab5/fir31.v
@@ -33,59 +33,90 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 module fir31(
-  input wire clock,reset,ready,
-  input wire signed [7:0] x,
-  output reg signed [17:0] y
-);
-// When ready is asserted, increment the offset and store the incoming data at sample[offset]. 
-// Set both the accumulator and index to 0.
-// Over the next 31 system clock cycles (@ 27MHz) compute coeff[index] * sample[offset-index], 
-// add the result to the accumulator, and increment the index. 
-// Remember to declare coeff and the sample memory as signed so that the multiply operation is performed correctly.
-// When index reaches 31, it's done and the accumulator contains the desired filter output! 
-// Now the module just waits until ready is asserted again and starts over.
+	input wire clock,reset,ready,
+	input wire signed [7:0] x,
+	output reg signed [17:0] y
+	);
+	// When ready is asserted, increment the offset and store the incoming data at sample[offset]. 
+	// Set both the accumulator and index to 0.
+	// Over the next 31 system clock cycles (@ 27MHz) compute coeff[index] * sample[offset-index], 
+	// add the result to the accumulator, and increment the index. 
+	// Remember to declare coeff and the sample memory as signed so that the multiply operation is performed correctly.
+	// When index reaches 31, it's done and the accumulator contains the desired filter output! 
+	// Now the module just waits until ready is asserted again and starts over.
 	reg signed [7:0] sample_buff [30:0];
 	reg signed [17:0] acc;
 	reg [4:0] offset = 0;
 	reg [4:0] ind = 0;
 	reg ready_prev;
-	
-  always @(posedge clock) 
-  begin
+
+	always @(posedge clock) 
+	begin
 		ready_prev <= ready;
-  end
-  
-  // for now just pass data through
-  always @(posedge clock) 
-  begin
-     if (reset)
-	  begin
+	end
+
+	// for now just pass data through
+	always @(posedge clock) 
+	begin
+		if (reset)
+		begin
+			sample_buff[1]  <= 8'sb0;
+			sample_buff[2]  <= 8'sb0;
+			sample_buff[3]  <= 8'sb0;
+			sample_buff[4]  <= 8'sb0;
+			sample_buff[5]  <= 8'sb0;
+			sample_buff[6]  <= 8'sb0;
+			sample_buff[7]  <= 8'sb0;
+			sample_buff[8]  <= 8'sb0;
+			sample_buff[9]  <= 8'sb0;
+			sample_buff[10] <= 8'sb0;
+			sample_buff[11] <= 8'sb0;
+			sample_buff[12] <= 8'sb0;
+			sample_buff[13] <= 8'sb0;
+			sample_buff[14] <= 8'sb0;
+			sample_buff[15] <= 8'sb0;
+			sample_buff[16] <= 8'sb0;
+			sample_buff[17] <= 8'sb0;
+			sample_buff[18] <= 8'sb0;
+			sample_buff[19] <= 8'sb0;
+			sample_buff[20] <= 8'sb0;
+			sample_buff[21] <= 8'sb0;
+			sample_buff[22] <= 8'sb0;
+			sample_buff[23] <= 8'sb0;
+			sample_buff[24] <= 8'sb0;
+			sample_buff[25] <= 8'sb0;
+			sample_buff[26] <= 8'sb0;
+			sample_buff[27] <= 8'sb0;
+			sample_buff[28] <= 8'sb0;
+			sample_buff[29] <= 8'sb0;
+			sample_buff[30] <= 8'sb0;
+			sample_buff[31] <= 8'sb0;
 			offset <= 0;
 			acc <= 0;
 			ind <= 0;
-	  end
-	  if ((~ready_prev) & ready)
-	  begin
+		end
+		if ((~ready_prev) & ready)
+		begin
 			acc <= 0;
 			ind <= 0;
-	      offset <= offset + 1;
+			offset <= offset + 1;
 			sample_buff[offset] <= x;
-	  end
-	  else if (&ind)
-	  begin
+		end
+		else
+		begin
+			acc <= acc + sample_buff[offset-ind-1]*coeffs31(ind);
 			y <= acc;
-	  end
-	  else
-	  begin
-			acc <= acc + sample_buff[offset-ind]*coeffs31(ind);
-			ind <= ind + 1;
-	  end
-  end
-  
+			if (~&ind)
+			begin
+				ind <= ind + 1;
+			end
+		end
+	end
+
 	function signed [9:0] coeffs31;
-	  input [4:0] index;
-	  begin
-		 case (index)
+	input [4:0] index;
+	begin
+		case (index)
 			5'd0:  coeffs31 = -10'sd1;
 			5'd1:  coeffs31 = -10'sd1;
 			5'd2:  coeffs31 = -10'sd3;
@@ -118,8 +149,8 @@ module fir31(
 			5'd29: coeffs31 = -10'sd1;
 			5'd30: coeffs31 = -10'sd1;
 			default: coeffs31 = 10'hXXX;
-		 endcase
-	  end
+		endcase
+	end
 	endfunction
 
 endmodule
---
+++ 6111work/Project/proposal.md
@@ -0,0 +1,9 @@
+# Sudoku Solver Using Verilog
+
+### Team: Magson Gao, Shreeyam Kacker
+
+## Project Description
+
+The aim of this project is to design and implement a sudoku solver using digital logic written in Verilog HDL. In order to implement this solver it is necessary that the digital logic is aware of the rules of sudoku, several key techniques for harder sudokus and when it is neccesary, how to guess values for cells in the sudoku grid. The techniques that will be implemented include, but are not limited to, the candidate line, naked group and hidden group techniques. Whilst these methods are sufficient for solving most novice to intermediate level sudokus, the most difficult sudokus require backtracking. The previous guesses will be stored on a stack based storage architecture to allow for previous states of the grid to be restored. The purpose of implementing the aforementioned techniques is to significantly reduce the search space required for guessing, allowing the solution to be found in fewer clock cycles and requiring less memory to store previous guesses for the typical sudoku. The FPGA that will be used is the Xilinx Nexys 4.
+
+The system will be evaluated by a testbench containing a set of multiple randomly generated sudokus for each difficulty level. Each sudoku will be generated using the QQWing sudoku generator (https://qqwing.com/generate.html) as it provides not only the sudoku itself but a list of the required algorithms used to solve the sudoku and the number of required guesses. The testbench will also include the "world's hardest sudoku" (Arto Inkala, 2010) which is one of the approximately, 50,000 minimum hint sudokus each of which contain only 17 of the 81 cells pre-filled in. 
